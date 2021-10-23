9251a3a6df4bc9272a6fceef8e5438ceb6b43d37
Manas <manas23601@gmail.com>
2021-10-23 20:00:31 +0530

    peer handshake error check

--- peerleech/parser.py
+++ peerleech/parser.py
@@ -33,7 +33,8 @@ class Torrent:
     
     #make a get request to the tracker to fetch the peer ips
     def get_from_tracker(self):
-
+        
+        # 20 byte long
         self.peer_id = hashlib.sha1(bytes(random.getrandbits(20))).digest()
         port = 6882
         left = self.file_length
@@ -58,7 +59,7 @@ class Torrent:
         self.run_connect()
     
     def run_connect(self):
-        self.peer_list[1].connect_to_peer(self.info_hash, self.peer_id)
+        self.peer_list[0].connect_to_peer(self.info_hash, self.peer_id)
 
 
 
--- peerleech/peer.py
+++ peerleech/peer.py
@@ -1,3 +1,4 @@
+from ctypes import resize
 import bcoding
 import hashlib
 import random
@@ -17,7 +18,10 @@ class Peer():
     
     def connect_to_peer(self, info_hash, peer_id):
         self.socket = socket.create_connection((self.ip, self.port), 3)
+        # requires peer_id which was sent initially to the tracker.
         message = struct.pack('>B19s8s20s20s', 19, b'BitTorrent protocol', b'\x00'*8, info_hash, peer_id)
         self.socket.sendall(message)
         result = struct.unpack(">B19s8s20s20s" ,self.socket.recv(1 + 19 + 8 + 20 + 20))
+        if result[4] != self.peer_id:
+            print("Error connecting to peer :", self.peer_id)
         print(result)
